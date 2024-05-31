58cdcdb5d43d89346d74caeafa019e3f8d4945ff
yohelperez <perezyohel@gmail.com>
2024-05-30 21:43:38 -0500

    first commit

---
+++ src/main/java/com/yohel/Main.java
@@ -0,0 +1,23 @@
+package com.yohel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        return new GreetResponse("Hello");
+    }
+
+    record GreetResponse(String greet){
+
+    }
+}
