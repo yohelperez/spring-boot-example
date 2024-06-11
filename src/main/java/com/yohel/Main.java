0560398c7f41da48017eda60b7f03a74adae7f12
yohelperez <perezyohel@gmail.com>
2024-06-10 21:39:20 -0500

    Complete CRUD operations for API

--- src/main/java/com/yohel/Main.java
+++ src/main/java/com/yohel/Main.java
@@ -1,23 +1,69 @@
 package com.yohel;
 
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final  CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet(){
-        return new GreetResponse("Hello");
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
 
-    record GreetResponse(String greet){
+    record NewCustomerRequest(Integer id,
+                              String name,
+                              String email,
+                              Integer age){
+
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id,
+                               @RequestBody NewCustomerRequest request){
+        Optional<Customer> optionalCustomer = customerRepository.findById(id);
+        if (optionalCustomer.isPresent()) {
+            Customer customer = optionalCustomer.get();
+            customer.setName(request.name());
+            customer.setEmail(request.email());
+            customer.setAge(request.age());
+            customerRepository.save(customer);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found");
+        }
 
     }
 }
